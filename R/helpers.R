66b74be60f5ec42a9658e13dcf6ec4f7193c6943
Justin Brantley <justinbrantley@me.com>
2018-08-04 03:11:31 +0000

    Update docs and exceptions

--- R/helpers.R
+++ R/helpers.R
@@ -1,16 +1,16 @@
 
 #' Dark Sky API Authentication
 #'
-#' These functions are used primarily for their side effects -- specifically to
+#' These functions are used specifically for their side effects -- namely to
 #' set (or reset) Dark Sky API authentication secrets stored in
 #' \code{options()$DarkSky}.
 #'
 #' @param key Dark Sky secret key
-#' @param lat latitude
-#' @param lon longitude
+#' @param lat latitude (in decimal degrees). Positive north; negative south.
+#' @param lon longitude (in decimal degrees). Positive east; negative west.
 #'
-#' @return Returns \code{TRUE} (invisibly) if a connection to the Hue Bridge was
-#'   successful.
+#' @return Returns \code{TRUE} (invisibly) if options were successfully set or
+#'   reset.
 #'
 #' @seealso \url{https://darksky.net/dev/docs}
 #'
@@ -28,7 +28,7 @@ set_dark_sky_credentials <- function(key, lat, lon) {
     }
 
     if (length(lon) != 1L || !is.numeric(lon) || lon < -180 || lon > 180) {
-        stop('Value for `lon` must be a single number in [-90, 90]')
+        stop('Value for `lon` must be a single number in [-180, 180]')
     }
 
     # Set options
@@ -39,4 +39,7 @@ set_dark_sky_credentials <- function(key, lat, lon) {
 
 #' @rdname set_dark_sky_credentials
 #' @export
-reset_dark_sky_credentials <- function() {options(DarkSky = list())}
+reset_dark_sky_credentials <- function() {
+    options(DarkSky = list())
+    return(invisible(TRUE))
+}
